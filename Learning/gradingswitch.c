7aee3bf68c747eaced6cc1daec6045f90f615bf0
Babanine <nophexer@gmail.com>
2024-06-11 18:33:16 +0200

    added gradingswitch.c

---
+++ Learning/gradingswitch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+    char grade;
+
+    printf("\nPlease enter your grade: ");
+    scanf("%c", &grade);
+
+    switch (grade)
+    {
+    case 'A':
+        printf("Perfect!\n");
+        break;
+    case 'B':
+        printf("Good job!\n");
+        break;
+    case 'C':
+        printf("Okay!\n");
+        break;
+    case 'D':
+        printf("At least you passed!\n");
+        break;
+    case 'F':
+        printf("You failed!\n");
+        break;
+    default:
+        printf("Please enter only valid grades (ABCDF)");
+        break;
+    }
+}
